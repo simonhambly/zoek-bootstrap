ee17e3d91260c6e2bbde7eee3e46d660b42fb74f
Simon Hambly <simon.hambly@unclouded.co.uk>
2017-07-28 10:57:03 +0100

    pull cli into a sub package

---
+++ post-install.js
@@ -0,0 +1,9 @@
+// install packages for cli
+// cut down version of https://strongloop.com/strongblog/modular-node-js-express/#easy-setup
+const cp = require('child_process')
+const { resolve } = require('path')
+
+console.log('Installing cli dependencies')
+
+const cwd = resolve(__dirname, './packages/cli/') 
+cp.spawn('npm', ['i'], { env: process.env, cwd, stdio: 'inherit' })
