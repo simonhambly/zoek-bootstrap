cce03d847de58858902e9c4dc00ab9f3965bae7d
Simon Hambly <simon.hambly@unclouded.co.uk>
2017-09-05 16:12:02 +0100

    include --no-optional in post-install

--- bin/post-install.js
+++ bin/post-install.js
@@ -10,4 +10,4 @@ const cwd = resolve(__dirname, '../packages/cli/')
 const opts = { env: process.env, cwd, stdio: 'inherit' }
 if (process.platform === 'win32') opts.shell = true
 
-cp.spawn('npm', ['i'], opts)
+cp.spawn('npm', ['install', '--no-optional'], opts)
